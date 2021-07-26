97242247c57cb4134868469104ccc369d107e947
Ricky Ho <36563085+ricky-ho@users.noreply.github.com>
2021-07-25 17:19:29 -0700

    Added Skills Component
    Added a Skills Component in Sections

---
+++ src/components/Sections/Skills/index.js
@@ -0,0 +1,32 @@
+import * as React from "react"
+
+import "./skills.scss"
+
+const Skills = () => {
+  return (
+    <section id="skills">
+      <div className="skills__inner container">
+        <div className="section__header">
+          <h2>Skills</h2>
+          <div></div>
+        </div>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>SASS</li>
+          <li>JavaScript</li>
+          <li>React</li>
+          <li>Gatsby</li>
+          <li>Node.js</li>
+          <li>MongoDB</li>
+          <li>Heroku</li>
+          <li>Netlify</li>
+          <li>Figma</li>
+          <li>Python</li>
+        </ul>
+      </div>
+    </section>
+  )
+}
+
+export default Skills
