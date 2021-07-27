5bda8c7e24d2b78aac143c316f0fc0adb060e02b
Ricky Ho <36563085+ricky-ho@users.noreply.github.com>
2021-07-26 19:12:27 -0700

    Mobile layout for Footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,20 +9,37 @@ const Footer = () => {
   return (
     <footer>
       <div className="footer__inner container">
-        <p>Copyright &copy; 2021 RICKY HO</p>
-        <a href="#" className="back-to-top">
-          <BiChevronUpCircle className="bi-chevron" size="45" />
+        <p>
+          &copy; 2021 <span>Ricky Ho</span>
+        </p>
+        <a href="#hero" className="back-to-top" title="Back to Top">
+          <BiChevronUpCircle className="bi-chevron" size={40} />
         </a>
 
         <div className="socials">
-          <a href="mailto:horicky2016@gmail.com">
-            <FiMail size={25} />
+          <a
+            href="https://github.com/ricky-ho"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="https://github.com/ricky-ho"
+          >
+            <FaGithub size={15} />
           </a>
-          <a href="https://github.com/ricky-ho">
-            <FaGithub size={25} />
+          <a
+            href="https://www.linkedin.com/in/ricky-ho-01/"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="https://www.linkedin.com/in/ricky-ho-01"
+          >
+            <FaLinkedinIn size={15} />
           </a>
-          <a href="https://www.linkedin.com/in/ricky-ho-01/">
-            <FaLinkedinIn size={25} />
+          <a
+            href="mailto:horicky.cs@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="mailto:horicky.cs@gmail.com"
+          >
+            <FiMail size={15} />
           </a>
         </div>
       </div>
