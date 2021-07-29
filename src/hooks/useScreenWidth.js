9a4c2108c3d1093af0162b7d87fa77f0eae223f0
Ricky Ho <36563085+ricky-ho@users.noreply.github.com>
2021-07-28 18:42:07 -0700

    Update useScreenWidth.js
    Added a condition to check if window is not undefined before using window.innerWidth for server side rendering issues

--- src/hooks/useScreenWidth.js
+++ src/hooks/useScreenWidth.js
@@ -1,12 +1,14 @@
 import { useState, useEffect } from "react"
 
 const useScreenWidth = () => {
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
-
-  const handleResize = () => setScreenWidth(window.innerWidth)
+  const [screenWidth, setScreenWidth] = useState(null)
 
   useEffect(() => {
+    if (typeof window === "undefined") return
+
+    const handleResize = () => setScreenWidth(window.innerWidth)
     window.addEventListener("resize", handleResize)
+
     return () => window.removeEventListener("resize", handleResize)
   }, [])
 
