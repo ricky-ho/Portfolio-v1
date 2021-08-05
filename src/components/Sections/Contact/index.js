b57d9bce8736ebe99acdee4438d85cd5ccfcfff9
Ricky Ho <36563085+ricky-ho@users.noreply.github.com>
2021-08-04 17:48:45 -0700

    Modified Contact section content and mobile layout

--- src/components/Sections/Contact/index.js
+++ src/components/Sections/Contact/index.js
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { IoMailUnreadOutline } from "react-icons/io5"
+import { IoMailUnreadOutline, IoPersonCircleOutline } from "react-icons/io5"
 
 import "../../../styles/contact.scss"
 
@@ -8,21 +8,63 @@ const Contact = () => {
     <section id="contact">
       <div className="section__header">
         <h2>Contact</h2>
-        <div></div>
       </div>
       <div className="contact__inner container">
-        <a
-          href="mailto:horicky.cs@gmail.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="email-card"
-        >
-          <IoMailUnreadOutline className="io-mail" />
-          <p>
-            <span>Email Me</span>
-            <span>horicky.cs@gmail.com</span>
-          </p>
-        </a>
+        <div className="contact__card">
+          <h2>Get In Touch</h2>
+          <a
+            href="mailto:horicky.cs@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IoMailUnreadOutline className="io-mail" size={45} />
+          </a>
+
+          <p>Email me at</p>
+          <p>horicky.cs@gmail.com</p>
+        </div>
+        <div className="contact__form-wrap">
+          <div className="form__inner">
+            <div className="form__header">
+              <IoPersonCircleOutline
+                className="io-person-circle"
+                color="white"
+                size={75}
+              />
+
+              <div>
+                <svg
+                  viewBox="0 0 100 100"
+                  width="10"
+                  height="10"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <circle cx="50" cy="50" r="50" fill="lime" />
+                </svg>
+                <span>Active</span>
+              </div>
+            </div>
+            <form action="" autoComplete="off">
+              <input type="name" id="name" placeholder="Your name*" required />
+              <input
+                type="email"
+                id="email"
+                placeholder="Your email*"
+                required
+              />
+              <input type="text" id="subject" placeholder="Subject*" required />
+              <textarea
+                name="message"
+                id="message"
+                rows="6"
+                placeholder="Your message*"
+                required
+              ></textarea>
+              <button type="submit">Send Message</button>
+            </form>
+          </div>
+        </div>
+        {/*
 
         <div className="form-wrap">
           <form action="" className="contact-form" autoComplete="off">
@@ -38,7 +80,7 @@ const Contact = () => {
             ></textarea>
             <button type="submit">Send</button>
           </form>
-        </div>
+        </div> */}
       </div>
     </section>
   )
