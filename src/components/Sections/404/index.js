44d0bddc171d118eb74f08de09fe79edea3a2486
Ricky Ho <36563085+ricky-ho@users.noreply.github.com>
2021-08-06 18:26:00 -0700

    Added custom 404 error page

---
+++ src/components/Sections/404/index.js
@@ -0,0 +1,16 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+import "../../../styles/404.scss"
+
+const NotFoundSection = () => {
+  return (
+    <section id="not-found-section">
+      <h1>404</h1>
+      <h2>Page Not Found</h2>
+      <Link to="/">Go Home</Link>
+    </section>
+  )
+}
+
+export default NotFoundSection
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,12 @@
 import * as React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/Layout"
 import Seo from "../components/seo"
+import { NotFoundSection } from "../components/Sections"
 
 const NotFoundPage = () => (
   <Layout>
     <Seo title="Page not found" />
-    <h1>404: Page Not Found</h1>
-    <Link to="/">Go back home</Link>
+    <NotFoundSection />
   </Layout>
 )
 
