dd28936d31f6ffc727688055fef2d123653e3095
Ricky Ho <36563085+ricky-ho@users.noreply.github.com>
2021-08-07 20:14:33 -0700

    Added favicon and moved images folder
    Removed Gatsby starter images, used logo.svg as favicon, and moved images folder into assets

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/assets/images`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -28,7 +28,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/images/logo.svg`, // This path is relative to the root of the site.
       },
     },
     {
