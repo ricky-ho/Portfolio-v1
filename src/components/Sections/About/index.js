38e6ba7ffa702df598c0215c9c6345fcf5c0ff3a
Ricky Ho <horicky.cs@gmail.com>
2021-09-10 21:12:35 -0700

    Update About section copy

--- src/components/Sections/About/index.js
+++ src/components/Sections/About/index.js
@@ -21,10 +21,10 @@ const About = () => {
         <div className="about__content">
           <div className="about-text">
             <p>
-              {`I am a recent graduate from UCLA with a B.S. in Mathematics of Computation, a combined mathematics & computer science degree. During my college career, I gained valuable technical skills, some of which include data structures & algorithms, computer organization & architecture, and computer graphics. I also have invaluable experience working in a collaborative environment and promoting positive team dynamics as the club president for a student-organized club.`}
+              {`I am a recent graduate from UCLA with a B.S. in Mathematics & Computer Science. During my college career, I gained valuable technical skills, some of which include data structures & algorithms, computer organization & architecture, and computer graphics. I also have invaluable experience working in a collaborative environment and promoting positive team dynamics as the club president for a student-organized club.`}
             </p>
             <p>
-              {`As a front-end software developer, I enjoy creating responsive and interactive web applications with simple, yet clean-looking designs. Although my main expertise is working on the frontend, I am also familiar with back-end tools and technologies including working with databases, RESTful APIs, and user authentication.`}
+              {`As a frontend software developer, I enjoy creating responsive and interactive web applications with simple, yet visually appealing designs. Although my main expertise is working on the frontend, I am also familiar with backend tools and technologies including working with databases, RESTful APIs, and user authentication.`}
             </p>
             <p>{`I always strive to learn and to improve in order to become both a better person and a better software developer.`}</p>
           </div>
